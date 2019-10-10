5fca7a7c0950c96dc8d67dc49fb89f9e99989346
Rico Mitchell <ricomitch@icloud.com>
2019-10-10 03:34:48 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Squawk!"
-  return "Squawk!" 
-end
+
 def parrot(phrase = "Squawk!")
   parrot_phrase = "#{phrase}"
   puts "#{phrase}"
